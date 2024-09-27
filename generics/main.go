0e7e56aebff571b7b7cd98e1ca70fa7a3eae66e8
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-09-27 15:25:38 +0330

    generics 2

--- generics/main.go
+++ generics/main.go
@@ -1,9 +1,26 @@
 package main
 
-type myInt int64
+import "strconv"
+
+// type myInt int64
+type Account[T Numbers] struct {
+	Balance T
+	Owner   string
+	// T --> Not allowed to embed
+}
+
+type FloatAccount = Account[float64]
+
+type Content interface {
+	~string | ~[]byte
+}
+
+type Reader[T Content] interface {
+	Read(content T) error
+}
 
 func main() {
-	// intSlice := []int{1, 2, 3, 4}
+	intSlice := []int{1, 2, 3, 4}
 	// strSlice := []string{"mmd", "asqar", "ahmad"}
 	// floatSlice := []float64{1.1, 2.2, 3.3}
 
@@ -16,36 +33,42 @@ func main() {
 	// 	return "converted -> " + strconv.Itoa(i)
 	// })
 
-
 	println(min(min(2, 3), min(3, 4)))
+
+	println(Reduce(intSlice, "", func (s int, result string) string  {
+		return result + " " + strconv.Itoa(s)  
+	}))
 }
 
 // Ype parametric programming --> generics
-func printElements[T any](s []T) {
-	for _, v := range s {
-		println(v)
-	}
-}
+// func printElements[T any](s []T) {
+// 	for _, v := range s {
+// 		println(v)
+// 	}
+// }
+
+// Instantiate generic types
+var MapInts = Map[int, string]
 
 func Map[T, U any](s []T, f func(T) U) []U {
-	resault := make([]U, len(s))
+	result := make([]U, len(s))
 
 	for i := range s {
-		resault[i] = f(s[i])
+		result[i] = f(s[i])
 	}
 
-	return resault
+	return result
 }
 
 // ~ = int or subtypes
 // type Ints interface {
-// 	~int | ~int8 | ~int16 | ~int32 | ~int64 
+// 	~int | ~int8 | ~int16 | ~int32 | ~int64
 // }
 
 type Numbers interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
-	~float32 | ~float64
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~float32 | ~float64
 }
 
 func min[T Numbers](a, b T) T {
@@ -72,3 +95,14 @@ func min[T Numbers](a, b T) T {
 // 	}
 // 	return b
 // }
+
+
+type reducerFunc[T, U any] func(currentElement T, currentResult U) U
+
+func Reduce[T, U any](s []T, init U, f reducerFunc[T, U]) U {
+	for i := range s {
+		init = f(s[i], init)
+	}
+
+	return init
+}
